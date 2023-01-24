8e59c733c2941b3430af7ee166ebe2d9c954602e
Jordan G <88185056+AbsoluteDev1@users.noreply.github.com>
2023-01-24 23:46:28 +0100

    ajout d'un tickmanager pour générer les boucles qui sont calé sur le tickrate comme les boucles pour les draw marker

--- absolute-tools/client/exemple/ExempleUtilisationZoneManager.lua
+++ absolute-tools/client/exemple/ExempleUtilisationZoneManager.lua
@@ -9,8 +9,13 @@ ZoneManager:createZone(vector3(-283.094513, -899.446167, 31.065918),5,function(p
     print("Je suis entré dans la zone !")
 end,function(player)
     print("Je suis dans la zone !")
+
+    TickManager:addTick("test",function()
+        print("Je suis dans la zone et je calé au tick rate on pourrait ici faire un DrawMarker!")
+    end)
 end,function(player)
     print("Je suis sorti de la zone !")
+    TickManager:removeTick("test")
 end);
 
 ZoneManager:createZone(vector3(-290.584625, -913.951660, 31.065918),5,function(player)
